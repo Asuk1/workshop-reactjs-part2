66bca153213d19902e55bf88280c165ed5254856
Julia TRAN <julia.tran@epitech.eu>
2022-05-12 16:50:57 +0200

    [ADD]Task3

--- workshop-reactjs-app/src/App.js
+++ workshop-reactjs-app/src/App.js
@@ -1,13 +1,18 @@
 import About from'./pages/About.js';
 import Home from'./pages/Home.js';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-          <Home />
-          <About />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
